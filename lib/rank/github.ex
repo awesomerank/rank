d21f7dab4ee7f0c993c9f1cf11b37e7af1aa15ed
denispeplin <denis.peplin@gmail.com>
2017-04-23 21:19:09 +0300

    Implement stargazers_count fetch.

--- lib/rank/github.ex
+++ lib/rank/github.ex
@@ -9,4 +9,10 @@ defmodule Rank.Github do
     |> :base64.decode
     |> String.split("\n")
   end
+
+  def get_stargazers_count(<<"https://github.com/", path::binary>>) do
+    [owner, repo] = String.split(path, "/")
+    %{"stargazers_count" => stargazers} = Tentacat.Repositories.repo_get(owner, repo)
+    stargazers
+  end
 end
