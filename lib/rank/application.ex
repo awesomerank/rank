5e0f4f277177ae057b405d87966e2dc053d4ce8e
denispeplin <denis.peplin@gmail.com>
2017-04-26 19:55:49 +0300

    Select specific Redis database (try to avoid cache destroy).

--- lib/rank/application.ex
+++ lib/rank/application.ex
@@ -12,7 +12,7 @@ defmodule Rank.Application do
     children = [
       # Starts a worker by calling: Rank.Worker.start_link(arg1, arg2, arg3)
       # worker(Rank.Worker, [arg1, arg2, arg3]),
-      worker(Redix, [[], [name: :redix]])
+      worker(Redix, [[database: 7], [name: :redix]])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
