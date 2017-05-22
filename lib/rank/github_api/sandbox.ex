68c72ff18ddb70e11850fd498b15e85a842b34d8
denispeplin <denis.peplin@gmail.com>
2017-05-22 21:35:40 +0300

    Cache Github repo data for tests.

--- lib/rank/github_api/sandbox.ex
+++ lib/rank/github_api/sandbox.ex
@@ -2,35 +2,42 @@ defmodule Rank.GithubApi.Sandbox do
   require Logger
 
   @readme_prefix "test/files/github/readme"
+  @repo_prefix "test/files/github/repo"
 
   def readme(owner, repo) do
     File.read!(readme_path(owner, repo))
   end
 
+  # use repo_get_record instead to get and record real Github repo data
   def repo_get(owner, repo) do
-    Tentacat.Repositories.repo_get(owner, repo, client())
+    @repo_prefix
+    |> Path.join(owner)
+    |> repo_path(repo)
+    |> File.read!
+    |> Poison.decode!
   end
-
-  defp client do
-    if token = System.get_env("AW_TOKEN") do
-      Tentacat.Client.new(%{
-        access_token: token
-      })
-    else
-      Logger.warn("Using unauthorized Github access. Export AW_TOKEN to raise limits.")
-      Tentacat.Client.new
-    end
+  def repo_get_record(owner, repo) do
+    info = Rank.GithubApi.Tentacat.repo_get(owner, repo)
+    data = info |> Poison.encode!(pretty: true)
+    dir = Path.join(@repo_prefix, owner)
+    File.mkdir_p(dir)
+    File.write!(repo_path(dir, repo), data)
+    info
   end
 
   defp path(owner, repo) do
     Path.join(owner, repo)
   end
 
-  def readme_path(owner, repo) do
+  defp readme_path(owner, repo) do
     Path.join([
       @readme_prefix,
       path(owner, repo),
       "readme.md"
     ])
+  end
+
+  defp repo_path(dir, repo) do
+    Enum.join([path(dir, repo), "json"], ".")
   end
 end
